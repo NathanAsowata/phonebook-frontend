dc7ad56ea496aa624bc3f20f08e6bccb7612df6a
Nathan Asowata <nathanasowata@gmail.com>
2022-12-30 10:06:35 +0100

    Completed phonebook frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Notification from './Notification'
+import phoneServices from "./services/phonebook"
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState("")
+  const [notificationMesssage, setNotificationMesssage] = (null)
+  
+
+  useEffect(() => {
+    phoneServices.getAll()
+    .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+  
+  const addNewPerson = (e) => {
+    e.preventDefault()
+    
+    const Found = persons.find(element => element.name === newName)
+    const NewPerson = {name: newName, number: newNumber}
+    
+    if(Found.name === newName) {
+      window.confirm(`${newName} already exists in the phonebook`)
+      phoneServices.updatePerson(Found.id, NewPerson)
+      setNewName("")
+      setNewNumber("")
+      window.location.reload()
+    }else{
+      phoneServices.newPerson(NewPerson)
+      setPersons(persons.concat(NewPerson))
+      setNotificationMesssage(`Added ${newName}`)
+      setNewName("")
+      setNewNumber("")
+    }
+
+  }
+
+  const deletePerson = (id, name) => {
+    window.confirm(`Delete ${name}?`)
+    phoneServices.deletePerson(id)
+    .then(setPersons(persons.filter(person => person.id !== id)))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={notificationMesssage} />
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: 
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          /><br />
+          number:
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, id) => {
+        return <li key={id}>
+                  {`${person.name} ${person.number}`}
+                  <button onClick={() => deletePerson(person.id, person.name)}>delete</button>
+                </li>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/services/phonebook.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+const baseUrl = `http://localhost:3001/persons`
+
+const getAll = () => {
+    const req = axios.get(baseUrl)
+    return req.then(res => res.data)
+}
+
+const newPerson = (newObject) => {
+    const req = axios.post(baseUrl, newObject)
+    return req.then(res => res.data)
+}
+
+const deletePerson = (id) => {
+    const req = axios.delete(`${baseUrl}/${id}`)
+    return req.then(res => res.data)
+}
+
+const updatePerson = (id, updatedObject) => {
+    const req = axios.put(`${baseUrl}/${id}`, updatedObject)
+    return req.then(res => res.data)
+}
+
+export default { getAll, newPerson, deletePerson, updatePerson }
